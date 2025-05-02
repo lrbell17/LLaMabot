ca87097b7488fd2156de2a49287f1bfc40261d60
lrbell17 <lukbell17@gmail.com>
2025-05-01 19:07:20 -0700

    chore: init project

---
+++ src/main/java/com/lrbell/llamabot/LlamabotApplication.java
@@ -0,0 +1,13 @@
+package com.lrbell.llamabot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlamabotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlamabotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lrbell/llamabot/LlamabotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lrbell.llamabot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlamabotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
